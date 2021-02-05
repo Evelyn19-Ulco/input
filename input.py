3e55fe517cb71b87efd66a8783decd5dd564fa12
Evelyn19-Ulco <76453340+Evelyn19-Ulco@users.noreply.github.com>
2021-02-05 11:46:38 -0500

    Add files via upload

---
+++ input.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 15:12:32 2021
+
+@author: DELL
+"""
+##leer=input("Ingrese su nombre:  ")
+##print ("hola", leer)
+print("Buenos dias","\n", input ("Ingrese su nombre: "),input ("Ingrese su apellido: "),"  solicitamos personas que tengan disponibilidad de tiempo y vivan en la provincia de Pichincha, de que ciudad nos contactamos ",input ("Ingrese la ciudad: "),"y que tengan la edad de " ,input ("Ingrese su edad:  años "), " años ,trabajo inmediato", "\n\n")
+
+
